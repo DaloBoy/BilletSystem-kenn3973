2d76913688605a346dce34253433d2ccfbafdc1f
Kenneth Walther Hansen <kenn3973@edu.easj.dk>
2017-08-30 12:15:39 +0200

    Opgave 1 Færdig

---
+++ BilletStorebeltTests/BilTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletStorebelt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletStorebelt.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void PrisTest240()
+        {
+            //Arrange
+            var bil = new Bil();
+            //Act
+            int pris = bil.Pris();
+            //Assert
+            Assert.AreEqual(240, pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTestBil()
+        {
+            //Arrange
+            var bil = new Bil();
+            //Act
+            string køretøj = bil.Køretøj();
+            //Assert
+            Assert.AreEqual("Bil", køretøj);
+        }
+    }
+}
